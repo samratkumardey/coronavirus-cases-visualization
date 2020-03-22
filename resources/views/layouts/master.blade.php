ae9fb034e02a1716511e6a6c2a3476c83deaf400
Ariful Islam <ariful.bpi2012@gmail.com>
2020-03-23 00:42:50 +0600

    add google analytics

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -54,6 +54,17 @@
         }
     </style>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-44838571-8"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-44838571-8');
+    </script>
+
+
     @yield('head-js')
 </head>
 <body>
